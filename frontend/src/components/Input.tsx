c67481b2bfd35560fa268cd189ea1edc5186d578
Emmy Manning <emswoo@EM.local>
2026-02-24 18:52:12 -0500

    new: created component

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,5 @@
+const Input = () => {
+  return <input type='text' className="border border-blue-400 bg-white rounded-sm p-2 w-full"/>
+};
+
+export default Input;
---
+++ frontend/src/components/RetrivalPanel.tsx
@@ -0,0 +1,9 @@
+const RetrivalPanel = () => {
+  return (
+    <div className='border border-blue-400 bg-white rounded-sm p-2 h-full'>
+      Retrival Results
+    </div>
+  );
+};
+
+export default RetrivalPanel;
