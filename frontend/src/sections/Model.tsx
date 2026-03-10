d9751c4923a4b0fdd1e2076cb921fb366dd2fc20
Emmy Manning <emswoo@EM.local>
2026-03-10 14:11:35 -0400

    new: add model type component

---
+++ frontend/src/sections/Model.tsx
@@ -0,0 +1,14 @@
+type ModelProps = {
+  modelData: string;
+};
+
+const Model = ({ modelData }: ModelProps) => {
+  return (
+    <div className="flex flex-col gap-1 ">
+      <h1 className="font-bold">Model:</h1>
+      <p>{modelData}</p>
+    </div>
+  );
+};
+
+export default Model;
