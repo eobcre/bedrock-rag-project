a90139bd27f91acc4d0ce3179dbf106a8948446e
Emmy Manning <emswoo@EM.local>
2026-03-10 12:40:32 -0400

    refactor: rename to Retrieval.tsx

--- frontend/src/components/RetrivalPanel.tsx
+++
@@ -1,9 +0,0 @@
-const RetrivalPanel = () => {
-  return (
-    <div className='border border-blue-400 bg-white rounded-sm p-2 h-full'>
-      Retrival Results
-    </div>
-  );
-};
-
-export default RetrivalPanel;
