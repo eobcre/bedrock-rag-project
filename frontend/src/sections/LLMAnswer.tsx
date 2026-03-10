f90f8e06d57e98021175f0c4094d24ec026c23ca
Emmy Manning <emswoo@EM.local>
2026-03-10 12:45:50 -0400

    refactor: for better structure

---
+++ frontend/src/sections/LLMAnswer.tsx
@@ -0,0 +1,14 @@
+type AnswerProps = {
+  answerData: string;
+};
+
+const LLMAnswer = ({ answerData }: AnswerProps) => {
+  return (
+    <div className="flex flex-col gap-1 border border-blue-400 bg-white rounded-sm p-2 h-full">
+      <h1 className="font-bold">LLM Answer:</h1>
+      <p>{answerData}</p>
+    </div>
+  );
+};
+
+export default LLMAnswer;
---
+++ frontend/src/sections/Retrieval.tsx
@@ -0,0 +1,9 @@
+const Retrival = () => {
+  return (
+    <div className='border border-blue-400 bg-white rounded-sm p-2 h-full'>
+      Retrival Results
+    </div>
+  );
+};
+
+export default Retrival;
