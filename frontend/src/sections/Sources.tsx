b960df863f55e0a178a320042edff32c4a05e8c2
Emmy Manning <emswoo@EM.local>
2026-03-11 13:29:37 -0400

    fix: wrap long source link to avoid horizontal scroll

--- frontend/src/sections/Sources.tsx
+++ frontend/src/sections/Sources.tsx
@@ -6,7 +6,7 @@ const Sources = ({ srcData }: SourceProps) => {
   const uri = srcData[0]?.s3Location?.uri;
 
   return (
-    <div className="flex flex-col gap-1 overflow-y-auto">
+    <div className="flex flex-col gap-1 break-all">
       <h1 className="font-bold">Sources:</h1>
       <p>{uri}</p>
     </div>
