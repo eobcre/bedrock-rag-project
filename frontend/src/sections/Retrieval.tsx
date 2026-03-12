4a9cfb32a3518f2d49af58f7aee3a22669db06fa
Emmy Manning <emswoo@EM.local>
2026-03-12 10:02:33 -0400

    style: improve ui

--- frontend/src/sections/Retrieval.tsx
+++ frontend/src/sections/Retrieval.tsx
@@ -4,7 +4,7 @@ type RetrievalProps = {
 
 const Retrieval = ({ retrievalData }: RetrievalProps) => {
   return (
-    <div className="flex flex-col gap-1 border border-blue-400 bg-white rounded-sm overflow-y-auto p-2 h-full">
+    <div className="flex flex-col gap-1 border border-blue-400 bg-white rounded-sm overflow-y-auto p-3 h-full">
       <h1 className="font-bold">Retrieval results:</h1>
       {retrievalData.map((chunk, index) => (
         <div key={index} className="bg-gray-100 rounded mb-2 p-2">
