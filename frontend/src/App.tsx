6878deaa8d5ebf9ac2557908cdd4b91c7e8c63bc
Emmy Manning <emswoo@EM.local>
2026-03-15 16:17:46 -0400

    refactor: move loader from App to separate component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Icon } from "@iconify/react";
 
 import Input from "./components/Input";
 import Dropdown from "./components/Dropdown";
+import Loader from "./components/Loader";
 import RetrievalSection from "./sections/Retrieval";
 import LLMAnswerSection from "./sections/LLMAnswer";
 import Metrics from "./sections/Metrics";
@@ -121,15 +122,7 @@ const App = () => {
       </section>
 
       {/* loader */}
-      {loading && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-200/70">
-          <div className="flex justify-center items-center space-x-3  h-screen dark:invert">
-            <div className="bg-gray-600 rounded-full animate-bounce [animation-delay:-0.3s] w-6 h-6"></div>
-            <div className="bg-gray-600 rounded-full animate-bounce [animation-delay:-0.15s] w-6 h-6"></div>
-            <div className="bg-gray-600 rounded-full animate-bounce w-6 h-6"></div>
-          </div>
-        </div>
-      )}
+      {loading && <Loader  />}
     </div>
   );
 };
