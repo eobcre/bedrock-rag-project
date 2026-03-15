12d8168bcb211fcf4453d5828a0df6dabffb6dbe
Emmy Manning <emswoo@EM.local>
2026-03-15 16:10:58 -0400

    feature: add loader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,6 +28,7 @@ const App = () => {
   const [active, setActive] = useState(false);
   const [error, setError] = useState(false);
   const [validationError, setValidationError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const topKOptions = [
     { label: "3", value: 3 },
@@ -45,6 +46,7 @@ const App = () => {
     }
 
     setValidationError("");
+    setLoading(true);
 
     try {
       const res = await fetch(`/api/rag`, {
@@ -67,6 +69,8 @@ const App = () => {
       console.error(err);
       setActive(false);
       setError(true);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -115,6 +119,17 @@ const App = () => {
           </div>
         </div>
       </section>
+
+      {/* loader */}
+      {loading && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-200/70">
+          <div className="flex justify-center items-center space-x-3  h-screen dark:invert">
+            <div className="bg-gray-600 rounded-full animate-bounce [animation-delay:-0.3s] w-6 h-6"></div>
+            <div className="bg-gray-600 rounded-full animate-bounce [animation-delay:-0.15s] w-6 h-6"></div>
+            <div className="bg-gray-600 rounded-full animate-bounce w-6 h-6"></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
